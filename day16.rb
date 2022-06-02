19ca40db686b20dd517e55435f6f960d71a4077c
Grace Joh <graceeunaejoh@gmail.com>
2022-06-01 23:52:55 -0400

    day16

---
+++ day16.rb
@@ -0,0 +1,14 @@
+def fibonacci(count)
+  number = 0
+  sequence = []
+  (0...count).each do |item|
+    number = item if item <= 1
+    number = sequence[-1] + sequence[-2] if item > 1
+    sequence << number
+
+    sum += number if number % 2 == 0
+  end
+  sum
+end
+
+puts fibonacci(8)
